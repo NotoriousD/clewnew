d1b98b60fb8ab4d51e3b4eafb216634e1a18448a
Danil_Kruhlikov <danylo.kruhlikov@nure.ua>
2020-02-08 16:43:57 +0200

    home desktop finish
    fix banner slider
    add humburger
    add description for banner slider
    add slider for colab + news
    add footer
    add contact form

--- app/js/common.js
+++ app/js/common.js
@@ -1,11 +1,18 @@
 $(function() {
 
+	$('.header__hamburger').on('click', function () {
+		$('.hamburger').toggleClass('is-active');
+		$('.header__menu-mobile').toggleClass('mobile-menu-active');
+		$('.bgmobile').toggleClass('bgmobile-active');
+	});
+
 	var bannerSlider = $('.slider').slick({
 		fade: true,
 		autoplay: true,
 		dots: true,
 		arrows: false,
-		
+		speed: 1000,
+		autoplaySpeed: 5000
 	});
 
 	$('.sl-prev').on('click', function(e){
@@ -16,6 +23,28 @@ $(function() {
 	$('.sl-next').on('click', function(e){
 		e.preventDefault();
 		bannerSlider.slick('slickNext');
+	});
+
+	var newsSlider = $('.news__slider').slick({
+		autoplay: true,
+		dots: false,
+		arrows: false,
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplaySpeed: 5000,
+		speed: 1000,
+	});
+
+	var colabSlider = $('.colab__slider').slick({
+		autoplay: true,
+		dots: false,
+		arrows: false,
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplaySpeed: 5000,
+		speed: 1000
 	});
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -46,7 +46,7 @@ gulp.task('scripts', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
 		'app/libs/slick/slick.min.js',
-		'app/js/common.js', // Always at the end
+		'app/js/common.js',
 		])
 	.pipe(concat('scripts.min.js'))
 	// .pipe(uglify()) // Mifify js (opt.)
